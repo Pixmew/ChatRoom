f29e93bd8054fa6d64fad961f96a7750f1952568
yash <universeyash4@gmail.com>
2020-09-30 19:09:09 +0530

    making use of pickels module
    by this way we can send any python objects through sockets

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import socket
+import pickel
 
 #size of heder which stores length of message
 header = 10
-
+d = {34:"sdiuf"}
 #create the server of stream type in ip4 format
 serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ip = socket.gethostbyname("localHost")
@@ -15,11 +16,11 @@ serversocket.listen(5)
 #continously check of avalible of clients
 while True:
     #server connection message
-    msg = "Welcome to the server"
+    msg = pickel.dumps(d)
     #accept all clients
     clientsocket,address = serversocket.accept()
     #concatnate the message with the size of msg in advanced
-    msg  = "{:<{header}}".format(len(msg),header = header) + msg
+    msg  = bytes("{:<{header}}".format(len(msg),header = header),"utf-8") + msg
     print(msg)
     print(f"Connection from {address} has been Established!")
     #send updated message to client
