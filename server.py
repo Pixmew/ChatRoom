47e7d3ad503f7c98742fb6fe4dcb386324ee8c61
yash <universeyash4@gmail.com>
2020-10-02 20:52:07 +0530

    Updated server and client
    Server:
    server is updated for handling multiple user not full features are implemented yet
    for now it just accept the new connection and parse their first message
    (their name)parse it and store it for further user
    
    client:
    all previous features were removed expect the actual connection
    procedure and client now takes user name and send its first message to Server
    (name of user)  for configuration for now continous loop is passed

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import socket
 import json
+import select
 
 #size of heder which stores length of message
 header = 10
-d = " Connection aquired\nhello client"
+d = " Connection aquired, You are now Connected to Server As a Chatter/Message Sender"
 #create the server of stream type in ip4 format
 serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 ip = socket.gethostbyname("localHost")
 print("connect to : "+ str(ip))
 #created server gets bind to ip and port
@@ -13,14 +15,46 @@ serversocket.bind((ip,3333))
 #sets que for sending and reciving data
 serversocket.listen(5)
 
+
+
+chatterList = [serversocket]
+seerList = []
+User = {}
+Intermidiatemsg = {}
+
+
+#Function which recieve msg from client and parse it
+def ReciveMessage(client_socket):
+    try:
+        msg = client_socket.recv(header)
+        print(msg)
+        msg_len = int(msg.decode().strip())
+        if not msg_len:
+            return False
+        msg = client_socket.recv(msg_len)
+        print(msg.decode())
+    except:
+        return False
+    return {"Header":str(msg_len) , "Data":msg.decode()}
+
+
+
 #continously check of avalible of clients
 while True:
-    #server connection message
-    msg = json.dumps(d)
-    #accept all clients
-    clientsocket,address = serversocket.accept()
-    #concatnate the message with the size of msg in advanced
-    msg  = "{:<{header}}".format(len(msg),header = header) + msg
-    print(f"Connection from {address} has been Established!")
-    #send updated message to client
-    clientsocket.send(msg.encode())
+    client_server , _ , client_exception = select.select(chatterList,[],chatterList)
+    #get Valid client
+    for notified_client in client_server:
+
+        #If there are any new connection accept that connection
+        if notified_client == serversocket:
+            client_socket , address = serversocket.accept()
+            msg = ReciveMessage(client_socket)
+            type = client_socket.recv(header)
+            print(type)
+            type = type.decode().strip()
+            print(type)
+            if type == "chat":
+                chatterList.append(client_socket)
+                User[address] = msg
+            print(chatterList)
+            print(User)
