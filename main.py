591f00bc547b952bdc769ff23cbe8de66d18afe4
Pixmew <universeyash4@gmail.com>
2020-11-21 06:12:59 -0800

    Added Window with two Buttons
    A window is added named Chatroom and Two Buttons are added named
    "Create" and "Join" not currently functional

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("main")
+import tkinter
+
+# Create a Main Window for GUI
+MainWindow = tkinter.Tk(className = "Chatroom")
+
+createButton = tkinter.Button(MainWindow , text = "Create" , activebackground = "#999")
+createButton.pack()
+
+joinButton = tkinter.Button(MainWindow , text = "Join" , activebackground = "#999")
+joinButton.pack()
+
+#End of loop of GUI
+MainWindow.mainloop()
