3247669ebd39fedfa992fd2b7158c057c14d2f12
Pixmew <universeyash4@gmail.com>
2020-11-28 07:40:08 -0800

    Cleared the window for changing layout

--- main.py
+++ main.py
@@ -15,17 +15,8 @@ ConnectionMenu = tkinter.Menu(menu)
 #adding menu elements
 menu.add_cascade(label = "Connection" , menu = ConnectionMenu)
 menu.add_command(label = "Exit" , command = MainWindow.quit)
-ConnectionMenu.add_command(label = "Create" ,command = )
+ConnectionMenu.add_command(label = "Create")
 ConnectionMenu.add_command(label = "Join")
-
-
-createButton = tkinter.Button(MainWindow , text = "Create" , activebackground = "#999" , command = CreateServer)
-createButton.pack()
-
-
-joinButton = tkinter.Button(MainWindow , text = "Join" , activebackground = "#999" , command = connect)
-joinButton.pack()
-
 
 #End of loop of GUI
 MainWindow.mainloop()
